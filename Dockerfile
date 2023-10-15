6a26f0e8131fa03fd81a678a64343fd5ac0343fa
bbjwz <82287672+bbjwz@users.noreply.github.com>
2023-10-15 20:12:05 +0000

    remove web framework

--- Dockerfile
+++ Dockerfile
@@ -1,25 +1,19 @@
 # Use a Python base image
 FROM python:3.8-slim-buster
 
-# Set environment variables for Django
+# Set environment variables for Python
 ENV PYTHONDONTWRITEBYTECODE 1
 ENV PYTHONUNBUFFERED 1
 
 # Set the working directory
 WORKDIR /app
 
-# Copy requirements and install dependencies
+# Install dependencies
 COPY requirements.txt /app/
 RUN pip install --no-cache-dir -r requirements.txt
 
-# Copy the rest of the project files
-COPY . /app/
+# Copy the source code into the container
+COPY src /app/src
 
-# Collect static files
-RUN python manage.py collectstatic --noinput
-
-# Expose the port the app will run on
-EXPOSE 8000
-
-# Use gunicorn to serve the application
-CMD ["gunicorn", "transcription.wsgi:application", "--bind", "0.0.0.0:8000"]
+# Set the entry point of the application
+ENTRYPOINT ["python", "src/transcribe.py"]
---
+++ src/transcribe.py
@@ -0,0 +1,27 @@
+import requests
+from pydub import AudioSegment
+import speech_recognition as sr
+
+def transcribe_audio(url):
+    response = requests.get(url, stream=True)
+    response.raise_for_status()
+
+    with open('audio_stream.wav', 'wb') as file:
+        for chunk in response.iter_content(chunk_size=8192):
+            file.write(chunk)
+
+    audio_file = AudioSegment.from_wav('audio_stream.wav')
+    audio_data = sr.AudioData(audio_file.raw_data, audio_file.frame_rate, audio_file.sample_width)
+
+    recognizer = sr.Recognizer()
+    try:
+        text = recognizer.recognize_google(audio_data)
+        print(f'Transcription: {text}')
+    except sr.UnknownValueError:
+        print("Could not understand audio")
+    except sr.RequestError as e:
+        print(f'Could not request results; {e}')
+
+if __name__ == '__main__':
+    AUDIO_URL = 'https://example.com/your-audio-file-url'
+    transcribe_audio(AUDIO_URL)
