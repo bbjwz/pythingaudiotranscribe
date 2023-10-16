9b34a968f9b02b94ac926d8f2c09312cd6e72258
bbjwz <82287672+bbjwz@users.noreply.github.com>
2023-10-16 18:26:08 +0000

    enhanced exception mgt

--- src/transcribe.py
+++ src/transcribe.py
@@ -2,37 +2,43 @@ import os
 import requests
 from pydub import AudioSegment
 import speech_recognition as sr
+import logging
+
+# Configure logging
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
 def transcribe_audio(url):
-    response = requests.get(url, stream=True)
-    print(response.status_code, flush=True)  # Should be 200
-    response.raise_for_status()
-    print('Status 200 is OK! Anything else not OK!')
-
-    with open('audio_stream.mp3', 'wb') as file:
-        for chunk in response.iter_content(chunk_size=8192):
-            file.write(chunk)
-
-    print('If you see this something at least line 12-14 were executed.')
-
-    print(f'File size: {os.path.getsize("audio_stream.mp3")} bytes', flush=True)  # Debug Step 2
-
-    audio_file = AudioSegment.from_mp3('audio_stream.mp3')  
-    print(audio_file, flush=True)  # Debug Step 3
-
-    audio_data = sr.AudioData(audio_file.raw_data, audio_file.frame_rate, audio_file.sample_width)
-    print(audio_data, flush=True)  # Debug Step 4
-
-
-    recognizer = sr.Recognizer()
     try:
+        response = requests.get(url, stream=True)
+        logging.debug(response.status_code)  # Should be 200
+        response.raise_for_status()
+
+        with open('audio_stream.mp3', 'wb') as file:
+            for chunk in response.iter_content(chunk_size=8192):
+                file.write(chunk)
+
+        logging.debug(f'File size: {os.path.getsize("audio_stream.mp3")} bytes')  # Debug Step 2
+
+        audio_file = AudioSegment.from_mp3('audio_stream.mp3')
+        logging.debug(audio_file)  # Debug Step 3
+
+        audio_data = sr.AudioData(audio_file.raw_data, audio_file.frame_rate, audio_file.sample_width)
+        logging.debug(audio_data)  # Debug Step 4
+
+        recognizer = sr.Recognizer()
         text = recognizer.recognize_google(audio_data)
-        print(f'Transcription: {text}')
+        logging.debug(f'Transcription: {text}')
+
     except sr.UnknownValueError:
-        print("Could not understand audio")
+        logging.error("Could not understand audio")
+
     except sr.RequestError as e:
-        print(f'Could not request results; {e}')
+        logging.error(f'Could not request results; {e}')
+
+    except Exception as e:
+        logging.error(f'Exception occurred: {e}')
+        logging.error("Exception", exc_info=True)  # This will log the full exception traceback
 
 if __name__ == '__main__':
-    AUDIO_URL = 'https://25703.live.streamtheworld.com/BNR_NIEUWSRADIO.mp3?dist=nederlandfm'
+    AUDIO_URL = 'https://stream.bnr.nl/bnr_mp3_128_03'
     transcribe_audio(AUDIO_URL)
